44470686a03b0ea9814ba607158e9a5b04272b25
Cleber-0101 <cleberbatista99@hotmail.com>
2026-05-27 16:31:56 -0300

    Estruturas de dados array

---
+++ Array/array.js
@@ -0,0 +1,24 @@
+var avgTemp = []
+
+avgTemp[1] = 53551.00
+avgTemp[2] = 8351.00
+avgTemp[3] = 534521.00
+avgTemp[4] = 423421.00
+avgTemp[5] = 4231.00
+avgTemp[6] = 42300000000000000.00
+
+//acessando posição do array
+console.log(avgTemp[1])
+console.log("####################################")
+
+//iniciando o array ja com dados definidos (mostra tamanho do array)
+var diasDaSemana = ['segunda', 'terça' , 'quarta' , 'quinta' , 'sexta', 'sabado' , 'domingo']
+
+//diasDaSemana[0] = 'cleber'
+
+console.log(diasDaSemana[0])
+console.log(diasDaSemana[1])
+console.log(diasDaSemana[2])
+console.log(diasDaSemana[3])
+console.log(diasDaSemana[5])
+console.log(diasDaSemana[6])
