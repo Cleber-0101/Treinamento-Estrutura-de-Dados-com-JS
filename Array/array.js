d931bb962edf4c07b01d51d27f69c37dbb72d5c2
Cleber-0101 <cleberbatista99@hotmail.com>
2026-05-30 12:49:52 -0300

    Arrays Bidimensionais

--- Array/array.js
+++ Array/array.js
@@ -7,18 +7,31 @@ avgTemp[4] = 423421.00
 avgTemp[5] = 4231.00
 avgTemp[6] = 42300000000000000.00
 
-//acessando posição do array
-console.log(avgTemp[1])
-console.log("####################################")
-
 //iniciando o array ja com dados definidos (mostra tamanho do array)
 var diasDaSemana = ['segunda', 'terça' , 'quarta' , 'quinta' , 'sexta', 'sabado' , 'domingo']
 
-//diasDaSemana[0] = 'cleber'
+//inserindo no final do array
+console.log(diasDaSemana.push("testando pop"))
 
-console.log(diasDaSemana[0])
-console.log(diasDaSemana[1])
-console.log(diasDaSemana[2])
-console.log(diasDaSemana[3])
-console.log(diasDaSemana[5])
-console.log(diasDaSemana[6])
+//pop remove items do array
+
+
+// removendo dados a parti de uma posição: preciso passar a partir de qual posição irei remover e quantos item irei remover
+diasDaSemana.splice(3,4)
+
+for(let i = 0; i < diasDaSemana.length ; i++){
+    console.log(diasDaSemana[i])
+}
+
+let fibonacci = []
+fibonacci[0] = 0
+fibonacci[1] = 1
+fibonacci[2] = 1
+
+for(let i = 3; i < 20 ; i++){
+    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2] 
+}
+
+for(var i = 0; i < fibonacci.length; i++){
+    console.log(fibonacci[i])
+}
---
+++ Array/bidimensionais.js
@@ -0,0 +1,13 @@
+
+// iniciando array vazia
+var avgTemp = []
+
+var avgTemperatura1 = [33,25.2,19,27,22.4]
+var avgTemperatura2 = [353,245.2,19,57,24.4]
+
+//Bidimensionais
+avgTemp[0] = avgTemperatura1
+avgTemp[1] = avgTemperatura2
+
+console.log(avgTemp[1][4])
+console.log(avgTemp)
