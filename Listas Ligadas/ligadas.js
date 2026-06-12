e7bd4ebb3dfb35ec8e7c8a011fa0800e8801adec
Cleber-0101 <cleberbatista99@hotmail.com>
2026-06-12 16:30:51 -0300

    Entendendo melhor como funciona lista ligadas

--- Listas Ligadas/ligadas.js
+++ Listas Ligadas/ligadas.js
@@ -3,4 +3,19 @@ function LinkedList(){
         this.element = element
         this.next = null
     }
+
+    var length = 0
+    var head = null 
+
+    this.append = function(){
+        //adiciona um elemente na fila 
+    }
+
+    this.insert = function(){
+        //Adciona um elemento em uma posição especifica 
+    }
+
+    this.removeAt = function(){
+        
+    }
 }
